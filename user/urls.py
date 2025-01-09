2a819a103cf4d733ba614e52eb7d058d9061b512
R Suvam Dora <155545062+rsuvamdora22@users.noreply.github.com>
2025-01-10 00:25:07 +0530

    Delete user directory

--- user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('registration',views.dora,name='reg'),
-    path('',views.log_in,name='login'),
-    path('logout',views.log_out,name='logout'),
-]
